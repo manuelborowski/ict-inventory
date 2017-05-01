aebc908d97280b8cac746f82ddf339a03cccdddb
manuel borowski <manuel.borowski@campussintursula.be>
2017-05-01 16:31:59 +0200

    backup

--- app/user/forms.py
+++ app/user/forms.py
@@ -8,7 +8,7 @@ from wtforms.validators import DataRequired, Email, EqualTo
 from ..models import User
 
 
-class RegistrationForm(FlaskForm):
+class UserForm(FlaskForm):
     """
     For users to create a new account
     """
@@ -20,6 +20,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password')])
     confirm_password = PasswordField('Confirm Password')
     submit = SubmitField('Register')
+    cancel = SubmitField('Cancel')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
--- app/user/views.py
+++ app/user/views.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 # app/user/views.py
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, render_template_string, flash, redirect, url_for
 from flask_login import login_required
 from flask_table import Table, Col
 
-from .forms import RegistrationForm
+from .forms import UserForm
 from .. import db
 from . import user
 from ..models import User
 
+
+#Special column to add html-tags.  Note : this can be dangerous, so whatch out!!!
+class NoEscapeCol(Col):
+    def td_format(self, content):
+        return content
 
 class UserTable(Table):
     id = Col('Id')
@@ -18,6 +23,8 @@ class UserTable(Table):
     first_name = Col('First name')
     last_name = Col('Last name')
     is_admin = Col('Admin')
+    delete = NoEscapeCol('')
+    edit = NoEscapeCol('')
     classes = ['table ' 'table-striped ' 'table-bordered ']
     html_attrs = {'id': 'usertable', 'cellspacing': '0', 'width': '100%'}
 
@@ -31,6 +38,9 @@ def users():
     List the users
     """
     users = User.query.all()
+    for u in users:
+        u.delete = render_template_string("<a href=\"{{ url_for('user.delete', id=" + str(u.id) + ") }}\"><i class='fa fa-trash'></i>")
+        u.edit = render_template_string("<a href=\"{{ url_for('user.edit', id=" + str(u.id) + ") }}\"><i class='fa fa-pencil'></i>")
     user_table = UserTable(users)
 
     return render_template('user/user.html', title='Users', user_table=user_table, table_id='usertable')
@@ -45,7 +55,7 @@ def add():
     Add a new user
     """
 
-    form = RegistrationForm()
+    form = UserForm()
     if form.validate_on_submit():
         user = User(email=form.email.data,
                         username=form.username.data,
@@ -63,3 +73,20 @@ def add():
 
     # load registration template
     return render_template('user/register.html', form=form, title='Register')
+
+
+#delete a user
+@user.route('/user/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+    print('delete user with id {}'.format(id))
+
+    return redirect(url_for('user.users'))
+
+#edit a user
+@user.route('/user/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    print('edit user with id {}'.format(id))
+
+    return redirect(url_for('user.users'))
