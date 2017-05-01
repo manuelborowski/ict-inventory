20f636de94c4024e5fe7a30dcc8d8a53b14b397a
manuel borowski <manuel.borowski@campussintursula.be>
2017-05-01 20:32:48 +0200

    add app/base.py

---
+++ app/base.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from wtforms.widgets.core import html_params
+from wtforms.widgets import HTMLString
+from wtforms import BooleanField
+
+
+class InlineButtonWidget(object):
+    """
+    Render a basic ``<button>`` field.
+    """
+    input_type = 'submit'
+    html_params = staticmethod(html_params)
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        kwargs.setdefault('type', self.input_type)
+        kwargs.setdefault('value', field.label.text)
+        return HTMLString('<input %s>' % self.html_params(name=field.name, **kwargs))
+
+
+class InlineSubmitField(BooleanField):
+    """
+    Represents an ``<button type="submit">``.  This allows checking if a given
+    submit button has been pressed.
+    """
+    widget = InlineButtonWidget()
